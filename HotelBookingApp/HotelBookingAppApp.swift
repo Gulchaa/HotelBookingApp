8d26141513104e48531de0a3c811d6a3657a15de
Gulchin Ibrahimli <ibragimli.gulchin@gmail.com>
2024-10-15 11:58:36 +0200

    Initial Commit

---
+++ HotelBookingApp/HotelBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingAppApp.swift
+//  HotelBookingApp
+//
+//  Created by Gulchin Ibragimli on 15.10.24.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
